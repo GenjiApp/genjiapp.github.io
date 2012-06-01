4d4885ebe4f0e32fb7801e456bb6f9493101e4fc
Genji <chaos.nonlinear@gmail.com>
2012-06-01 10:35:58 +0900

    add new release Murasaki ver. 2.0

---
+++ mac/murasaki/js/slideshow.js
@@ -0,0 +1,26 @@
+$(window).load(function() {
+    $('#slideShow ul li:not(:first-child)').hide();
+    var $img = $('#slideShow ul li:first-child img');
+    var width = $img.width();
+    $('#slideShow ul').css('width', width + 'px');
+
+    var numberOfChildren = $('#slideShow ul li').length;
+    var $ol = $('#slideShow').append('<ol/>').children('ol');
+    for(var ii = 0; ii < numberOfChildren; ii++) {
+        $ol.append('<li/>');
+    }
+    $('#slideShow ol li:nth-child(1)').addClass('active');
+
+    $('#slideShow ol li').click(function() {
+        var index = $(this).parent('ol').children().index(this) + 1;
+        var $li = $('#slideShow ul li:nth-child(' + index + ')');
+        $('#slideShow ul').css({
+            'width': $li.width() + 'px',
+            'height': $li.height() + 'px'
+        });
+        $('#slideShow ol li.active').removeClass('active');
+        $(this).addClass('active');
+        $('#slideShow ul li').hide();
+        $li.fadeIn();
+    });
+});
